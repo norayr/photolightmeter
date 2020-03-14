cd7e62702b1536d22d364cdc7a1ee8d1c2fbfa3b
Norayr Chilingarian <norayr@arnet.am>
2020-03-14 17:28:56 +0400

    added n900 light meter line for maemo-leste. i hope it'll compile, did not check.

--- lux.pas
+++ lux.pas
@@ -13,7 +13,7 @@ function GetLight() : integer;
 var f : textfile;
  // s : string;
   z, i : integer;
-luxpaths : array [0..2] of string;
+luxpaths : array [0..3] of string;
 luxfile : string;
 found : boolean;
 begin
@@ -23,10 +23,11 @@ luxpaths[0] := '/sys/class/i2c-adapter/i2c-0/device/0-0029/lux';
 luxpaths[1] := '/sys/class/i2c-adapter/i2c-2/2-0029/lux';
 // n900 MeeGo 1.1
 luxpaths[2] := '/sys/class/i2c-adapter/i2c-2/2-0029/device0/illuminance0_input';
-
+// n900 maemo-leste
+luxpaths[3] := '/sys/class/i2c-adapter/i2c-2/2-0029/iio:device1/in_illuminance0_input';
 //   GetLight := 555;
 found := false;
-  for i := 0 to 2 do begin
+  for i := low(luxpaths) to high(luxpaths) do begin
      luxfile := luxpaths[i];
      if SysUtils.FileExists(luxfile) then begin
         found := true;
